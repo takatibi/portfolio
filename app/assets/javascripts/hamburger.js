0151199464c5a68cfa51c34b3be0fbf2dcab0d80
takatibi <taka123nyw@gmail.com>
2020-04-22 11:13:21 +0000

    deploy setting

---
+++ app/assets/javascripts/hamburger.js
@@ -0,0 +1,9 @@
+
+$(function(){
+  $('#menu_link').click(function(){
+    $('.icon_link_tab').addClass('open');
+  });
+  $('.close_btn').click(function(){
+    $('.icon_link_tab').removeClass('open');
+  });
+});
