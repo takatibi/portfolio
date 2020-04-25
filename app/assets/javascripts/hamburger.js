72ef0ac01e5202cbff7c6ac4ed591452a2f007b9
takatibi <taka123nyw@gmail.com>
2020-04-25 04:41:11 +0000

    app/assets/javascripts/hamburger.js内ファイル削除

--- app/assets/javascripts/hamburger.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-  $('#menu_link').click(function(){
-    $('.icon_link_tab').addClass('open');
-  });
-  $('.close_btn').click(function(){
-    $('.icon_link_tab').removeClass('open');
-  });
-});
