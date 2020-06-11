8b3e72ff84a34f4ab6048453a7128bc69584415c
takatibi <taka123nyw@gmail.com>
2020-06-11 15:51:11 +0000

    [update]バーガー機能がない画面で、検証画面でエラーが出ていたので条件分岐で解消

--- app/assets/javascripts/hamburger.js
+++ app/assets/javascripts/hamburger.js
@@ -4,12 +4,15 @@ $(function(){
 	  var hamburger = document.getElementById('js-hamburger');
 	  var blackBg = document.getElementById('js-black-bg');
 
-	  hamburger.addEventListener('click', function() {
-	    body.classList.toggle('nav-open');
-	  });
-	  blackBg.addEventListener('click', function() {
-	    body.classList.remove('nav-open');
-	  });
+	  	if(hamburger){
+			hamburger.addEventListener('click', function() {
+	    	body.classList.toggle('nav-open');
+	  		});
+
+	  		blackBg.addEventListener('click', function() {
+	    	body.classList.remove('nav-open');
+	  		});
+		}
 	}
 
 	toggleNav();
